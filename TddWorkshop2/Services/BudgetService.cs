33d40022b82cd79cef5899607bb6e32edda604c2
En Yeh <en.yeh@titansoft.com.sg>
2023-12-21 17:05:05 +0800

    add test query partial month pass by hard code

--- TddWorkshop2/Services/BudgetService.cs
+++ TddWorkshop2/Services/BudgetService.cs
@@ -2,8 +2,14 @@
 
 public class BudgetService
 {
-    public decimal Query(DateTime dateTime, DateTime dateTime1)
+    public decimal Query(DateTime start, DateTime end)
     {
+        var daysInMonth = DateTime.DaysInMonth(start.Year, start.Month);
+        var daysPeriod = (end - start).Days+1;
+        if (daysPeriod < daysInMonth)
+        {
+            return 3100 * daysPeriod / daysInMonth;
+        }
         return 3100;
     }
 }
